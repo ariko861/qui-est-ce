fce1e3da86e3cf963f03fa18afffefe215d73a1f
Arthur Briquet <mdpbriar@henallux.be>
2023-02-17 14:37:46 +0100

    Ajout des images pour personnes

--- script.js
+++ script.js
@@ -6,12 +6,33 @@ document.addEventListener('alpine:init', () => {
         questions: [],
         personnes: [],
         count: 0,
-        questionFirstColumn: 1,
+        questionFirstColumn: 2,
         personnesRestantes: true,
         options: [],
+        images: [],
+
+
+        populateImages(){ // fonction pour remplir la liste d'images des images du excel
+            this.personnes.forEach(personne => {
+                this.images.push({
+                    prenom: personne.prenom,
+                    url: personne.image,
+                    show: true
+                })
+            });
+        },
+
+        checkImages(){
+            this.images.forEach(image => {
+                let correspondant = this.personnes.find(personne => personne.prenom == image.prenom);
+                if ( !correspondant ) {
+                    image.show = false;
+                }
+            });
+        },
 
         checkIfLeft() { // vérifie si il reste plus d'une personne dans la liste
-            if ( this.personnes.length == 1 ) {
+            if (this.personnes.length == 1) {
                 this.personnesRestantes = false; // si il ne reste plus qu'une personne mettre personnesRestantes à false
             } else {
                 this.count++; // Sinon passer à la question suivante
@@ -19,30 +40,29 @@ document.addEventListener('alpine:init', () => {
         },
 
         answer(bool) {
-            console.log(bool);
-            this.personnes = this.personnes.filter(function(personne) { // filtre les personnes selon la réponse donnée 
+            this.personnes = this.personnes.filter(function (personne) { // filtre les personnes selon la réponse donnée 
                 return personne[this.questions[this.count]] == bool;
             }, this);
             this.checkIfLeft(); // vérifier le nombre de personnes restantes
-            if ( this.personnesRestantes ) {
-                this.checkAvailableOptions(); // vérifier les options disponibles pour la question
-            }
+            this.checkAvailableOptions(); // vérifier les options disponibles pour la question
+            this.checkImages();
+            console.log(this.images);
         },
 
-        checkAvailableOptions(){
+        checkAvailableOptions() {
             this.options = []
             this.personnes.forEach(personne => {
                 let option = personne[this.questions[this.count]];
-                if ( this.options.indexOf(option) == -1 ) {
+                if (this.options.indexOf(option) == -1) {
                     this.options.push(option);
                 }
             }, this);
-            if ( this.options.length < 2 ) {
+            if (this.options.length < 2 && this.personnesRestantes) {
                 this.answer(this.options[0]);
             }
         },
 
-        async getFile(files){
+        async getFile(files) {
             file = files[0]; // sélectionne le premier fichier
             const data = await file.arrayBuffer(); // converti en buffer
             var workbook = XLSX.read(data, { // lit le buffer avec un lecteur xlsx
@@ -52,26 +72,27 @@ document.addEventListener('alpine:init', () => {
             this.readWorkbook(workbook);
         },
 
-        async fetchFile(url){
+        async fetchFile(url) {
             let response = await (await fetch(url)).arrayBuffer();
             var workbook = XLSX.read(response);
             this.readWorkbook(workbook);
         },
 
-        readWorkbook(workbook){ // Lit le fichier excel une fois déposé
-            
-            
+        readWorkbook(workbook) { // Lit le fichier excel une fois déposé
+
+
             var rows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[workbook.SheetNames[0]]); // converti les lignes du excel en objets
-            
+
             var keys = Object.keys(rows[0]); // récupère les clés 
-            for ( let i = this.questionFirstColumn; i < keys.length ; i++ ){ // assigne les clés aux questions à poser, commence par la colonne définie dans questionFirstColumn
+            for (let i = this.questionFirstColumn; i < keys.length; i++) { // assigne les clés aux questions à poser, commence par la colonne définie dans questionFirstColumn
                 this.questions.push(keys[i]);
             }
-          
+
             rows.forEach(element => { // ajoute chaque ligne à la liste de personnes
                 this.personnes.push(element);
             });
             this.checkAvailableOptions();
+            this.populateImages();
         },
 
 
